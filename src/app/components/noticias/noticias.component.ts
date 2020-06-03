3b7eac2b8ca635855417bcdfe89981bc3d28671c
Jose Ganchozo <joseganchozo@outlook.com>
2020-06-03 07:47:14 -0500

    add notice and notices components

---
+++ src/app/components/noticias/noticias.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { NoticiasService } from 'src/app/services/noticias.service';
+import { Article } from 'src/app/interfaces/interfaces';
+
+@Component({
+  selector: 'app-noticias',
+  templateUrl: './noticias.component.html',
+  styleUrls: ['./noticias.component.scss'],
+})
+export class NoticiasComponent implements OnInit {
+
+  @Input() noticias: Article[] = [];
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
